ed6ba46a12d65a846b4edd6a72d533ef9c2b5c98
Tanaka Jimha <tajimha@microsoft.com>
2020-08-19 12:24:42 -0700

    package references

--- PhotoStoreDemo/StartUp.cs
+++ PhotoStoreDemo/StartUp.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Diagnostics;
-using Windows.Management.Deployment;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.ApplicationModel.DataTransfer.ShareTarget;
